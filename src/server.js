99be538b3c0e74451b3a1f352f80eb4e97fe0e8c
MainBank5 <eliudkaruga97@gmail.com>
2025-03-21 23:14:58 +0300

    added docker.yml and dockerFile

--- src/server.js
+++ src/server.js
@@ -14,13 +14,13 @@ const pool = new Pool({
 });
 
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public"))); // ✅ Correct static folder path
 
-// Serve HTML Frontend
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html")); // ✅ Updated to match new src location
 });
@@ -35,7 +35,7 @@ app.get("/items", async (req, res) => {
     }
 });
 
-// Add new item
+
 app.post("/items", async (req, res) => {
     try {
         const { name } = req.body;
